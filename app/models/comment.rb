45f5c0d11a13e9fcf10bc660eb7266d36071c15c
osaboy <osazuwa@gmail.com>
2014-03-24 19:16:53 -0700

    Created User, Comment, Post and Category models & migrations.

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ActiveRecord::Base
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+  belongs_to :post 
+  #do not have to define foreign key because the assumptions are correct. Class is Post
+end
