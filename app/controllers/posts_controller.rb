0159a5bb3d1847e0c57f1abf5eaf7506e851720f
osaboy <osazuwa@gmail.com>
2014-03-29 18:27:20 -0700

    Completed the index action, show action, index view and show view for the Postit Template Application.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,29 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render :show  #this would have happened by default anyways
+  end
+
+  def new #show the post form
 
   end
+
+  def create #handle the submission of the new post form
+
+  end
+
+  def edit #display the edit post form
+
+  end
+
+  def update #handle the submission of that edit post form
+ 
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 PostitTemplate::Application.routes.draw do
-  root to: 'posts#index'
+  root to: 'post#index'
+  resources :posts, except: [:destroy]
 end
