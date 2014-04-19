bd42e46341a15f6acb12e4395bc30a05fc06637d
osaboy <osazuwa@gmail.com>
2014-04-18 20:41:20 -0700

    Bug Fixes
    Fixed a bug which prevents a user from voting on both a post and it’s
    associated comments. In other words now a user can vote on a post, and
    then vote on any comments associated with the comment.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,8 +3,6 @@ class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
 
   #using the scope parameter to limit this validation to just voteable columns
-  validates_uniqueness_of :creator, scope: :voteable
-  # can also do the following
   validates_uniqueness_of :creator, scope: [:voteable_id, :voteable_type]
 
 end
