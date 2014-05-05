bff09fdb8e8d59df30ef963b0d582332bcc1620a
osaboy <osazuwa@gmail.com>
2014-05-04 20:39:20 -0700

    Extract common slugging code
    extract common slugging code into a single module.
    I should note here, that my User and Category modules require
    uniqueness, the slug functionality is overkill for these modules.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   
   include Voteable
+  include Sluggable
 
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
@@ -13,40 +14,10 @@ class Post < ActiveRecord::Base
   validates :url, presence: true
   validates :description, presence: true
 
-  before_save :generate_slug!
+  # moved the code below to the lib/sluggable.rb file
+  # before_save :generate_slug!
   #use before_create if you don't want the slug to be regenerated
 
-  def to_param
-    self.slug
-  end
-
-  # ! to denote a destructive action
-  def generate_slug!
-    the_slug = to_slug(self.title)
-    post = Post.find_by(slug: the_slug)
-    count = 2
-    while post && post != self # don't want to append for an existing post object,
-    # e.g while editing and updating
-      the_slug = append_slug(the_slug, count)
-      post = Post.find_by(slug: the_slug)
-      count += 1
-    end
-    self.slug = the_slug.downcase
-  end
-
-  def append_slug(str, count)
-    if str.split('-').last.to_i != 0
-      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
-    else
-      return str + "-" + count.to_s
-    end
-  end
-
-  def to_slug(str)
-    str = str.strip
-    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
-    str.gsub! /-+/,"-"
-    str.downcase
-  end
-
+  sluggable_column :title
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Sluggable
+
   has_many :posts
   has_many :comments
   has_many :votes
@@ -8,39 +10,10 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 6}
 
-  before_save :generate_slug!
+  # moved the code below to the lib/sluggable.rb file
+  # before_save :generate_slug!
+  #use before_create if you don't want the slug to be regenerated
 
-  def to_param
-    self.slug
-  end
-  
-  # ! to denote a destructive action
-  def generate_slug!
-    the_slug = to_slug(self.username)
-    user = User.find_by(slug: the_slug)
-    count = 2
-    while user && user != self # don't want to append for an existing user object, 
-    # e.g while editing and updating
-      the_slug = append_slug(the_slug, count)
-      user = User.find_by(slug: the_slug)
-      count += 1
-    end
-    self.slug = the_slug.downcase
-  end
-
-  def append_slug(str, count)
-    if str.split('-').last.to_i != 0
-      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
-    else
-      return str + "-" + count.to_s
-    end
-  end
-
-  def to_slug(str)
-    str = str.strip
-    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
-    str.gsub! /-+/,"-"
-    str.downcase
-  end
+  sluggable_column :username
 
 end
